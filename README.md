44f07ae37935ac8075dc3f913cc329ce36ed2ad1
mithrayls <mithray@protonmail.com>
2020-07-31 07:12:39 +1200

    working to two levels

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fun and Fast CLI
+
+Fun, fast, and easy cli scaffolding that can be configured in yaml with recursive subcommands.
--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
 
-const commander = require('commander')
 const path = require('path')
 const fs = require('fs')
+const { Command } = require('commander')
+var program = new Command()
 const YAML = require('yaml')
-const XML = require('fast-xml-parser')
 
-const program = new commander.Command()
 
 function readCommandFile( commandsFilePath ){
     const data = fs.readFileSync(commandsFilePath,{ encoding: 'utf-8' })
@@ -15,48 +14,34 @@ function readCommandFile( commandsFilePath ){
         commands = JSON.parse(data)
     } else if (commandsFilePath.endsWith('.yml')) {
         commands = YAML.parse(data)
-    } else if (commandsFilePath.endsWith('.xml')) {
-        let tObj = parser.getTraversalObj(data,{})
-        commands = parser.convertToJson(tObj,{})
     }
+
     return commands
 }
 
-function addCommands(program, commands){
-  commands.forEach( command => {
-    const action = require(path.join(process.cwd(), command.action) + '.js')
-    var newCommand = createCommand(command)
-    program.addCommand(newCommand)
-    if ( command.subcommands ){
-//            console.log(`subcommand is ${JSON.stringify(subcommand)}`)
-//            const newCommand = createCommand(command.action)
-        addCommands(program, command.subcommands)
-    }
-  })
-}
-
-function createCommand(command_data){
-
-    const newCommand = new commander.Command(command_data.command)
-
-    console.log(command_data)
-
-    newCommand
-      .command(command_data.command)
-      .description(command_data.description)
-      .action(command_data.action)
-
-    return newCommand
-
-}
-
-function createProgram(commandsFilePath){
-
+function createProgram(commandsFilePath) {
     var commands = readCommandFile(commandsFilePath)
-    addCommands(program,commands)
-    program.parse(process.argv);
+    commands.forEach((command) => {
+    if(command.subcommands){
+        const newCommand = program.command(command.command)
 
+        command.subcommands.forEach((subcommand)=>{
+        const actionFile = path.join( process.cwd(), subcommand.action) + '.js'
+        const action = require(actionFile)
+        newCommand
+            .command(subcommand.command)
+            .description(subcommand.description)
+            .action(action)
+        })
+    } else {
+        const actionFile = path.join(process.cwd(), command.action) + '.js'
+        const action = require(actionFile)
+        program
+            .command(command.command)
+            .description(command.description)
+            .action(action)
+        }
+    })
     return program
 }
-
 module.exports = createProgram
